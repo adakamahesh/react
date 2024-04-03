ea1447813d64b357b71fa044b2ec6bc0f494cbad
adakamahesh <adakamahesh@gmail.com>
2024-04-03 21:48:57 +0530

    modified props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-let title="Guntur Kaaram";
-let imgurl="https://upload.wikimedia.org/wikipedia/en/3/3e/Guntur_Kaaram_film_poster.jpg";
-
+let ssmb28={
+   title:"Guntur Kaaram",
+   imgurl:"https://upload.wikimedia.org/wikipedia/en/3/3e/Guntur_Kaaram_film_poster.jpg",
+   description:" Guntur Kaaram: Directed by Trivikram Srinivas. With Sreeleela, Mahesh Babu, Jayaram, Ramya Krishnan. Years after his mother abandons him and remarries, ..."
+}
+let pages={
+  title:"18pages",
+  imgurl:"https://m.media-amazon.com/images/M/MV5BY2Q1ZjBjMTMtNjQxOS00OTRkLThmZjUtZWUyMzVhMTA4MGE1XkEyXkFqcGdeQXVyNTgxODY5ODI@._V1_.jpg",
+  description:"18 Pages (2022) photos, including production stills, premiere photos and other event photos, publicity photos, behind-the-scenes, and more"
+}
+ let Aadikeshava={
+  title:"Aadikeshava",
+  imgurl:"https://content.tupaki.com/en/feeds/2023/11/24/205400-aadikeshava.webp",
+  description:"Aadikeshava is a 2023 Indian Telugu-language action drama film directed by Srikanth N. Reddy in his directorial debut. The film stars Panja Vaisshnav Tej, .."
+ }
 function App() {
   const firstname="mahesh";
   return (
@@ -8,28 +20,36 @@ function App() {
     <div className="App">
       <h1> React Page </h1>
       <h3>hello {new Date().toLocaleTimeString()}</h3>
-      <p>
-      The export default keywords specify the main component in the file. If you’re not familiar with some piece of JavaScript syntax, MDN and javascript.info have great references. 
-      </p>
     </div>
 
-
-    <section className="Movie-container">
-      <Movie/>
+    {/* props */}
+    <section className="movie-container">
+      <Movie title={ssmb28.title} imgurl={ssmb28.imgurl} description={ssmb28.description}/>
+      <Movie title={pages.title} imgurl={pages.imgurl} description={pages.description}/> 
+      <Movie title={Aadikeshava.title} imgurl={Aadikeshava.imgurl} description={Aadikeshava.description}/>    
     </section>
     </div>
   );
 }
 
-const Movie=()=>{
+const Movie=(props)=>{
   return(
     <article>
+      <h2>{props.firstname}</h2>
+      <h2>{props.lastname}</h2>
       <img
-      src={imgurl}
+      src={props.imgurl}
       alt="NetFlix Movie"
       />
-      <h1>{title}</h1>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
     </article>
   );
 }
+
+// function getdata(firstname,lastname){
+//   console.log(firstname);
+//   console.log(lastname);
+// }
+// getdata("mahesh","babu");
 export default App;
