3c265f768fd996f67cba437e8c213352c74705f3
adakamahesh <adakamahesh@gmail.com>
2024-05-07 00:00:47 +0530

    use state ex 3

--- src/use-state-hooks/UseState-ex-3.jsx
+++ src/use-state-hooks/UseState-ex-3.jsx
@@ -6,6 +6,10 @@ const Index = () => {
         text:"",
         id:"",
     });
+    const [editingItem,seteditingItem]=useState({
+        id:"",
+        isEditing:false
+    });
 
     const changemessage = (e) => {
         setmessage({
@@ -13,9 +17,62 @@ const Index = () => {
             text:e.target.value
         });
     };
-    const handelsubmit = (e) =>{
+    const handelSubmit = (e) =>{
         e.preventDefault();
-    }
+        let newTodo ={
+            text:message.text,
+            id:new Date().getTime().toString()
+        }
+        setlist([...list,newTodo]);//this is used to display the add element
+        setmessage({
+            text:"",
+            id:""
+        });
+    };
+
+    const handleDelete = (id) =>{
+        let newTodo =list.filter((eachItem)=>{
+            return eachItem.id !== id
+        });
+        setlist(newTodo);
+    };
+    const changeEditState=(id)=>{
+
+        seteditingItem({
+            ...editingItem,
+            id:id,
+            isEditing:true
+        });
+        let editableItem=list.find((eachItem)=>eachItem.id===id)
+        setmessage({
+            ...message,
+            text:editableItem.text,
+            id:editableItem.id
+        });
+        console.log(editableItem);
+    };
+    const handelEdit=(e)=>{
+        e.preventDefault();
+        let newTodos=list.map((eachItem)=>{
+            if(eachItem.id===editingItem.id){
+                return{
+                    text:message.text,
+                    id:editingItem.id
+                };
+            } else{
+                return eachItem;
+            }
+        });
+        setlist(newTodos);
+        setmessage({
+            text:"",
+            id:"",
+        });
+        seteditingItem({
+            id:"",
+            isEditing:false,
+        });
+    };
 
   return (
     <div>
@@ -27,17 +84,22 @@ const Index = () => {
               value={message.text}
               onChange={changemessage}
             />
-            <button onClick={handelsubmit} type='submit'>add</button>
+            {
+                editingItem.isEditing?<button onClick={handelEdit} type='submit'>edit</button>:<button onClick={handelSubmit} type='submit'>add</button>
+            }
         </form>
         <hr/>
+        {
+            list.length === 0 && <h3>There is no items in the list</h3>
+        }
         <ul>
             {
                 list.map((eachItem) =>{
                     const {text,id}=eachItem;
                     return <li key={id}>
                         <span>{text}</span>
-                        <button>edit</button>
-                        <button>Delete</button>
+                        <button onClick={()=>changeEditState(id)}>edit</button>
+                        <button onClick={()=> handleDelete(id)}>Delete</button>
                     </li>
                 })
             }
